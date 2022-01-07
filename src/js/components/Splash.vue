ff1fe5b85c328c38ade6c1d60c17e3ed4e4bd861
Lui <mlsenica@gmail.com>
2022-01-07 15:46:00 +0800

    Updates

--- src/js/components/Splash.vue
+++ src/js/components/Splash.vue
@@ -2,7 +2,7 @@
     <div style="padding-top: 50px;">
         <div class="heading">
             <img src="https://uploads-ssl.webflow.com/6172279814cf5440b9aec966/61af04bce7f1b7da9d7a5396_Thrivelab_Hello_b5qvbt_wrtcqb.svg">
-            <h2 class="h2-md">
+            <h2 class="h2-md" style="margin-top: 10px;">
                 NICE TO MEET YOU
             </h2>
             <h1 class="h1-intro h1-lg" style="max-width: 350px; line-height: 1.2; font-size: 25px;">
