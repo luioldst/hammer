92069889690b306df07ceb0c64d0910be3849f3b
Lui <mlsenica@gmail.com>
2022-01-21 18:08:11 +0800

    Free visit params

--- src/js/components/FreeVisit.vue
+++ src/js/components/FreeVisit.vue
@@ -85,6 +85,8 @@ export default {
                     
                     this.getCMSData();
                 } );
+            } else {
+                window.location.href = '/';
             }
 
             
